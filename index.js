426ce3cc9e4a1c21e55db419fc336a22c7214cc9
Ally M <alexandra.miklos.i@gmail.com>
2019-09-03 19:39:17 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("Hi");
+localStorage.setItem("name", "Victoria");
+console.log(localStorage.getItem("name"));
+localStorage.removeItem('name');
+localStorage.setItem("name", "puccini");
+console.log(localStorage.getItem("name"));
